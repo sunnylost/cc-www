9ebf7fa004ebd3b4fe890e37b7a2a3f02245ec6c
immissile <shaohualou@gmail.com>
2014-02-24 01:30:36 +0800

    add team img to res.js

--- source/public/javascripts/common/res.js
+++ source/public/javascripts/common/res.js
@@ -9,7 +9,14 @@
       banner_d: '/images/cc-tech-1.jpg',
       banner_e: '/images/detail-javaee.jpg',
       banner_f: '/images/detail-html5.jpg',
-      banner_g: '/images/detail-safety.jpg'
+      banner_g: '/images/detail-safety.jpg',
+      team_a: '/images/team/1.png',
+      team_b: '/images/team/2.png',
+      team_c: '/images/team/3.png',
+      team_d: '/images/team/4.png',
+      team_e: '/images/team/5.png',
+      team_f: '/images/team/6.png',
+      team_g: '/images/team/7.png'
     };
   });
 
